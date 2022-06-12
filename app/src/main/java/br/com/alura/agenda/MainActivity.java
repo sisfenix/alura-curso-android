e77dca03e70ffb8a4ef21d747e0c7a6d0a382663
Alan Lopes <desenv.alanlopes@gmail.com>
2022-06-11 21:29:57 -0300

    Implementando o layout da Activity

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -10,9 +10,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Toast.makeText(this, "Alan Lopes", Toast.LENGTH_LONG).show();
-        TextView aluno = new TextView(this);
-        aluno.setText("Alan Lopes");
-        setContentView(aluno);
+//        Toast.makeText(this, "Alan Lopes", Toast.LENGTH_LONG).show();
+//        TextView aluno = new TextView(this);
+//        aluno.setText("Alan Lopes");
+//        setContentView(aluno);
+        setContentView(R.layout.activity_main);
     }
 }
