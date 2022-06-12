41dc0d91a8d26f88fa58b8bed2e7ba0c80bd12db
Alan Lopes <desenv.alanlopes@gmail.com>
2022-06-11 21:23:06 -0300

    TextView

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -3,12 +3,16 @@ package br.com.alura.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Alan Lopes", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Alan Lopes", Toast.LENGTH_LONG).show();
+        TextView aluno = new TextView(this);
+        aluno.setText("Alan Lopes");
+        setContentView(aluno);
     }
 }
