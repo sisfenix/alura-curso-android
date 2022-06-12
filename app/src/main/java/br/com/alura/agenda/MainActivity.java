f9220605cfbc8021d12ca320fc327f4aa3f76b98
Alan Lopes <desenv.alanlopes@gmail.com>
2022-06-11 21:46:28 -0300

    Utilizando AdapterViews para coleções de views

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,17 +18,24 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        Toast.makeText(this, "Alan Lopes", Toast.LENGTH_LONG).show();
+//        -------------------------------------------------------------
 //        TextView aluno = new TextView(this);
 //        aluno.setText("Alan Lopes");
 //        setContentView(aluno);
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(
-                Arrays.asList("Alex", "Fran", "Jose"));
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno  = findViewById(R.id.textView2);
-        TextView terceiroAluno = findViewById(R.id.textView3);
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+                Arrays.asList("Alex", "Fran", "Jose", "Maria", "Ana"));
+//        TextView primeiroAluno = findViewById(R.id.textView);
+//        TextView segundoAluno  = findViewById(R.id.textView2);
+//        TextView terceiroAluno = findViewById(R.id.textView3);
+//        primeiroAluno.setText(alunos.get(0));
+//        segundoAluno.setText(alunos.get(1));
+//        terceiroAluno.setText(alunos.get(2));
+        //findViewById(R.id.activity_main_lista_de_alunos)
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
     }
 }
